b2043cb48b46bcb52a9668d531ac9e68d4e3891c
Agil Atakishiyev <aqilatakishiyev1@gmail.com>
2022-06-23 11:11:51 +0400

    fix db issue

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,27 @@
 module.exports = {
-  HOST: process.env.HOST || "localhost",
-  USER: process.env.User || "",
-  PASSWORD: process.env.Password || "123",
-  DB: process.env.Database || "testdb",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  development: {
+    HOST: "localhost",
+    USER: "",
+    PASSWORD: "123",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  production: {
+    production: {
+      use_env_variable: "DATABASE_URL",
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
   },
 };
